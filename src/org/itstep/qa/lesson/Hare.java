d249e902619b5f757584846f6229999df5c5a6c6
qa <xbx@jn.dd>
2018-10-02 21:17:44 +0300

    HomeWork

---
+++ src/org/itstep/qa/lesson/Hare.java
@@ -0,0 +1,61 @@
+package org.itstep.qa.lesson;
+
+public class Hare {
+    private double born;
+    private double born1;
+    private double now;
+    private double now1;
+    private double prig;
+    private double kolprig;
+
+
+    public Hare (double born, double born1, double now, double now1, double prig, double kolprig){
+        this.born = born;
+        this.born1 = born1;
+        this.now = now;
+        this.now1 = now1;
+        this.prig = prig;
+        this.kolprig = kolprig;
+    }
+
+
+
+    public void Prigok (double nd, double ns){
+        now += nd;
+        now1 += ns;
+    }
+
+
+    public double getBorn() {
+        return born;
+    }
+
+    public void setBorn(double born) {
+        this.born = born;
+    }
+
+    public double getBorn1() {
+        return born1;
+    }
+
+    public void setBorn1(double born1) {
+        this.born1 = born1;
+    }
+
+    public double getNow() {
+        return now;
+    }
+
+    public void setNow(double now) {
+        this.now = now;
+    }
+
+    public double getNow1() {
+        return now1;
+    }
+
+    public void setNow1(double now1) {
+        this.now1 = now1;
+    }
+
+}
---
+++ src/org/itstep/qa/runner/HareRunner.java
@@ -0,0 +1,9 @@
+package org.itstep.qa.runner;
+
+import org.itstep.qa.lesson.Hare;
+
+public class HareRunner {
+    public static void main(String[] args) {
+        Hare hare = new Hare(55,33, 5 , 3);
+    }
+}
